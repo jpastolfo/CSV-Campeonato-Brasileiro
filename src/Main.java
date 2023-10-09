9ff4049d6477b4737f199d08c9cd7a3a59681e35
Guilherme Alleoni <guilherme.alleoni@yahoo.com.br>
2023-10-08 22:20:49 -0300

    Adicionando o Jogador que mais fez gols

--- src/Main.java
+++ src/Main.java
@@ -19,47 +19,71 @@ public class Main {
         System.out.println("\n======= PROJETO FINAL =======");
         System.out.println("=== CAMPEONATO BRASILEIRO ===\n");
 
-        Entry<String,Integer> vencedor = getTeamWithMostWins(fullDataPath);
+        Entry<String, Integer> vencedor = getTeamWithMostWins(fullDataPath);
         System.out.println("O time que mais venceu jogos no ano 2008 foi " +
-                vencedor.getKey().replace("\"","") +
+                vencedor.getKey().replace("\"", "") +
                 ", com " + vencedor.getValue() + " vitórias.");
 
         Entry<String, Integer> estado = getStateWithLessGames(fullDataPath);
         System.out.println("O estado com menos jogos entre 2003 e 2022 foi " +
-                estado.getKey().replace("\"","") + ", com " + estado.getValue() + " jogos.");
+                estado.getKey().replace("\"", "") + ", com " + estado.getValue() + " jogos.");
+
+        Entry<String, Integer> golsPorJogador = getPlayerWithMostGols(goalsDataPath);
+        System.out.println("O jogador que mais fez gols é o: " +
+                golsPorJogador.getKey().replace("\"", "") + ", com " + golsPorJogador.getValue() + " gols");
 
 
     }
 
     private static Entry<String, Integer> getStateWithLessGames(Path fullDataPath) throws IOException {
-        Map<String,Integer> jogosPorEstado = new HashMap<>();
+        Map<String, Integer> jogosPorEstado = new HashMap<>();
         Files.lines(fullDataPath)
                 .skip(1)
-                .filter(lines -> isBetweenYears(lines,2003,2022))
+                .filter(lines -> isBetweenYears(lines, 2003, 2022))
                 .forEach(lines -> {
                     String[] dadosLinha = lines.split(",");
-                    jogosPorEstado.put(dadosLinha[14],jogosPorEstado.getOrDefault(dadosLinha[14],0)+1);
+                    jogosPorEstado.put(dadosLinha[14], jogosPorEstado.getOrDefault(dadosLinha[14], 0) + 1);
                 });
-        Entry<String,Integer> minEntry =
+        Entry<String, Integer> minEntry =
                 Collections.min(jogosPorEstado.entrySet(),
                         Entry.comparingByValue());
         return minEntry;
     }
 
     private static Entry<String, Integer> getTeamWithMostWins(Path fullDataPath) throws IOException {
-        Map<String,Integer> vitoriasPorTime = new HashMap<>();
+        Map<String, Integer> vitoriasPorTime = new HashMap<>();
         Files.lines(fullDataPath)
                 .skip(1)
-                .filter(lines -> isInYear(lines,2008))
+                .filter(lines -> isInYear(lines, 2008))
                 .filter(Main::isTeamValid)
                 .forEach(lines -> {
-                        String[] dadosLinha = lines.split(",");
-                        vitoriasPorTime.put(dadosLinha[10],vitoriasPorTime.getOrDefault(dadosLinha[10],0)+1);
+                    String[] dadosLinha = lines.split(",");
+                    vitoriasPorTime.put(dadosLinha[10], vitoriasPorTime.getOrDefault(dadosLinha[10], 0) + 1);
                 });
-        Entry<String,Integer> maxEntry =
+        Entry<String, Integer> maxEntry =
                 Collections.max(vitoriasPorTime.entrySet(),
-                                Entry.comparingByValue());
+                        Entry.comparingByValue());
         return maxEntry;
+    }
+
+    private static Entry<String, Integer> getPlayerWithMostGols(Path goalsDataPath) throws IOException {
+        Map<String, Integer> jogadorComMaisGols = new HashMap<>();
+        Files.lines(goalsDataPath).skip(1)
+                .filter(Main::isPlayerValid)
+                .forEach(lines -> {
+                    String[] dadosLinha = lines.split(",");
+                    jogadorComMaisGols.put(dadosLinha[3], jogadorComMaisGols.getOrDefault(dadosLinha[3], 0) + 1);
+                });
+        Entry<String, Integer> maxEntry =
+                Collections.max(jogadorComMaisGols.entrySet(),
+                        Entry.comparingByValue());
+        return maxEntry;
+    }
+
+    private static boolean isPlayerValid(String lines){
+        int columnPlayer = 3;
+        String[] lineData = lines.split(",");
+        return (!lineData[columnPlayer].equals("\"-\""));
     }
 
     private static boolean isTeamValid(String lines) {
@@ -71,17 +95,37 @@ public class Main {
     private static boolean isInYear(String lines, int yearToCheck) {
         int columnDate = 2;
         String[] lineData = lines.split(",");
-        LocalDate data = LocalDate.parse(lineData[columnDate].replace("\"",""),
-                                        DateTimeFormatter.ofPattern("d/M/yyyy"));
+        LocalDate data = LocalDate.parse(lineData[columnDate].replace("\"", ""),
+                DateTimeFormatter.ofPattern("d/M/yyyy"));
         return data.getYear() == yearToCheck;
     }
 
-    private static boolean isBetweenYears(String lines,int startingYear,int endingYear) {
+    private static boolean isBetweenYears(String lines, int startingYear, int endingYear) {
         int columnDate = 2;
         String[] lineData = lines.split(",");
-        LocalDate data = LocalDate.parse(lineData[columnDate].replace("\"",""),
-                                         DateTimeFormatter.ofPattern("d/M/yyyy"));
+        LocalDate data = LocalDate.parse(lineData[columnDate].replace("\"", ""),
+                DateTimeFormatter.ofPattern("d/M/yyyy"));
         return (data.getYear() >= startingYear || data.getYear() <= endingYear);
     }
 
-}
+
+   /* private static Entry<String, Integer> getTeamWithMostWins(Path fullDataPath) throws IOException {
+        Map<String,Integer> vitoriasPorTime = new HashMap<>();
+        Files.lines(fullDataPath)
+                .skip(1)
+                .filter(lines -> isInYear(lines,2008))
+                .filter(Main::isTeamValid)
+                .forEach(lines -> {
+                    String[] dadosLinha = lines.split(",");
+                    vitoriasPorTime.put(dadosLinha[10],vitoriasPorTime.getOrDefault(dadosLinha[10],0)+1);
+                });
+        Entry<String,Integer> maxEntry =
+                Collections.max(vitoriasPorTime.entrySet(),
+                        Entry.comparingByValue());
+        return maxEntry;
+
+    */
+
+    }
+
+
